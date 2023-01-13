0357c2ed5cb120aa81401be751562fefc8326f4b
wangqinghua <867075698@qq.com>
2023-01-13 11:28:44 +0800

    Initial commit

---
+++ lesson01/lesson001.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time     : 2023/1/13 11:27
+# @Author   : wqh
+# @Email    : 867075698@qq.com
+# @File     : lesson001.py
+# @Software : PyCharm
+
+
+print("hello world")
